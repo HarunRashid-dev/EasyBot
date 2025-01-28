9b64da4aba591f7e206ad9c53ade4fb50db1a4fd
Admin <harunrashid9159@gmail.com>
2025-01-28 08:56:26 +0530

    Code Completed till 7:36

--- app/src/main/java/np/com/bimalkafle/easybot/ui/theme/ChatPage.kt
+++ app/src/main/java/np/com/bimalkafle/easybot/ui/theme/ChatPage.kt
@@ -1,10 +1,40 @@
 package np.com.bimalkafle.easybot.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 @Composable
 fun ChatPage(modifier: Modifier = Modifier){
-    Text(text = "Chat")
+    Column (
+        modifier = modifier
+    ){
+        AppHeader()
+    }
+}
+
+@Composable
+fun AppHeader(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+    ){
+     Text(
+         modifier = Modifier.padding(16.dp),
+         text = "Easy Bot",
+         color = Color.White,
+         fontSize = 22.sp
+     )
+    }
 }
