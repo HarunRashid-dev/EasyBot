3ccd9b9b183d91d05d9aa442b04e63f1128e4eab
Admin <harunrashid9159@gmail.com>
2025-01-28 08:22:30 +0530

    Code Completed till 6:36

---
+++ app/src/main/java/np/com/bimalkafle/easybot/ui/theme/ChatPage.kt
@@ -0,0 +1,10 @@
+package np.com.bimalkafle.easybot.ui.theme
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatPage(modifier: Modifier = Modifier){
+    Text(text = "Chat")
+}
